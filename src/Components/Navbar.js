542b5f111596cb4986d976f388d22723a8409a1e
june jepkirui <jepkiruijune@gmail.com>
2024-08-14 16:31:18 +0300

    added navbar-feature

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavBar.css';
+import { Link } from 'react-router-dom';
 
-function NavBar() {
+function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-        <li><NavLink to="/events" activeClassName="active">Available Events</NavLink></li>
-        <li><NavLink to="/about" activeClassName="active">About Us</NavLink></li>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/events">Available Events</Link></li>
+        <li><Link to="/about">About Us</Link></li>
       </ul>
     </nav>
   );
 }
 
-export default NavBar;
+export default Navbar
